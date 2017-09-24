fde63e7454b7534b10ac2680e98bdc2313a1fcbc
fxjzzyo <fxjzzyo@gmail.com>
2017-09-24 18:36:53 +0800

    weather02_v2, add title.

--- app/src/main/java/cn/edu/pku/zhangqixun/minweather/MainActivity.java
+++ app/src/main/java/cn/edu/pku/zhangqixun/minweather/MainActivity.java
@@ -1,13 +1,16 @@
 package cn.edu.pku.zhangqixun.minweather;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //去掉标题栏
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.weather_info);
     }
 }
